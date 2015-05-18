05bad8453277d1a88fe3a537b7ec198da466d6c6
Abdullah P Rashed Ahmed <dd.apra93@gmail.com>
2015-05-17 21:27:15 -0400

    adding CinCout

---
+++ CinCout/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string input;
+
+    cout << "Enter your name:\n\n";
+
+    cin >> input;
+
+    cout << "\nYour name is " << input << "\nAren't I so smart...\n";
+}
